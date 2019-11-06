e67cc491cd1296ddb19b671caa3b152bb2a2700e
Rohit Suratekar <rohitsuratekar@gmail.com>
2019-11-06 11:39:11 +0100

    Combined ROC curves

--- analysis/tpm.py
+++ analysis/tpm.py
@@ -9,16 +9,14 @@ from pprint import pprint as pp
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from SecretColors import Palette
+from matplotlib.lines import Line2D
 
 from constants.boolean import *
-from constants.ngs import *
-from constants.system import GENOTYPE_WT
-from helpers.ngs_parser import average_data, get_string_tie_data
+from helpers.ngs_parser import *
 
-# RANDOM_SEED = 1610
-# np.random.seed(RANDOM_SEED)
+RANDOM_SEED = 1610
+np.random.seed(RANDOM_SEED)
 pd.options.mode.chained_assignment = None  # This needed to suppress warning
 
 
@@ -38,9 +36,14 @@ def get_negative_control():
     return genes
 
 
-def get_data(hour):
-    data = get_string_tie_data(GENOTYPE_WT, hour, BIO_PROJECT_WINATA_LAB)
-    return average_data(data, OUTPUT_STRING_TIE)
+def get_data(hour, output_method):
+    if output_method == OUTPUT_STRING_TIE:
+        data = get_string_tie_data(GENOTYPE_WT, hour, BIO_PROJECT_WINATA_LAB)
+    elif output_method == OUTPUT_SALMON:
+        data = get_salmon_data(GENOTYPE_WT, hour, BIO_PROJECT_WINATA_LAB)
+    else:
+        raise Exception("Invalid output method")
+    return average_data(data, output_method)
 
 
 class TPMAnalysis:
@@ -49,7 +52,8 @@ class TPMAnalysis:
         self._raw_negative = get_negative_control()
 
         self.test_percentage = 25
-        self.iterations = 100
+        self.divisions = 100
+        self.output_method = OUTPUT_STRING_TIE
 
         self._positive_control = None
         self._positive_test = None
@@ -90,18 +94,35 @@ class TPMAnalysis:
             self._distribute_genes()
         return self._negative_test
 
-    @staticmethod
-    def calculate_error(data, tpm_threshold, pos_genes, neg_genes):
-        pos = data[data[STRING_GENE_NAME].isin(pos_genes)]
-        neg = data[data[STRING_GENE_NAME].isin(neg_genes)]
+    @property
+    def gene_column(self):
+        if self.output_method == OUTPUT_STRING_TIE:
+            return STRING_GENE_NAME
+        elif self.output_method == OUTPUT_SALMON:
+            return SALMON_GENE_NAME
+        else:
+            raise Exception(f"Unknown output method: {self.output_method}")
 
-        pos[pos[STRING_TPM] >= tpm_threshold] = 1
-        neg[neg[STRING_TPM] >= tpm_threshold] = 1
-        pos[pos[STRING_TPM] < tpm_threshold] = 0
-        neg[neg[STRING_TPM] < tpm_threshold] = 0
+    @property
+    def tpm_column(self):
+        if self.output_method == OUTPUT_STRING_TIE:
+            return STRING_TPM
+        elif self.output_method == OUTPUT_SALMON:
+            return SALMON_TPM
+        else:
+            raise Exception(f"Unknown output method: {self.output_method}")
 
-        pos_err = len(pos) - pos[STRING_TPM].sum()
-        neg_err = neg[STRING_TPM].sum()
+    def calculate_error(self, data, tpm_threshold, pos_genes, neg_genes):
+        pos = data[data[self.gene_column].isin(pos_genes)]
+        neg = data[data[self.gene_column].isin(neg_genes)]
+
+        pos[pos[self.tpm_column] >= tpm_threshold] = 1
+        neg[neg[self.tpm_column] >= tpm_threshold] = 1
+        pos[pos[self.tpm_column] < tpm_threshold] = 0
+        neg[neg[self.tpm_column] < tpm_threshold] = 0
+
+        pos_err = len(pos) - pos[self.tpm_column].sum()
+        neg_err = neg[self.tpm_column].sum()
         return tpm_threshold, pos_err + neg_err, pos_err, neg_err
 
     def _calculate_rates(self, pos_err, neg_err):
@@ -115,9 +136,9 @@ class TPMAnalysis:
         return tpr, fpr
 
     def analyze(self, data):
-        d = data[[STRING_GENE_NAME, STRING_TPM]]
+        d = data[[self.gene_column, self.tpm_column]]
         errs = []
-        for t in np.linspace(0, 10, 500):
+        for t in np.linspace(1, 10, self.divisions):
             errs.append(self.calculate_error(d, t,
                                              self.positive_control,
                                              self.negative_control))
@@ -134,35 +155,56 @@ class TPMAnalysis:
         return best_tpm, distance, rates
 
 
-def plot_roc_curve(hour, iterations=100):
-    p = Palette()
-    data = get_data(hour)
-    values = []
+def get_plot_values(data, iterations, divisions, output_method):
+    values_string = []
     for _ in range(iterations):
         t = TPMAnalysis()
-        values.append(t.analyze(data))
+        t.divisions = divisions
+        t.output_method = output_method
+        values_string.append(t.analyze(data))
 
-    values = sorted(values, key=lambda m: m[1])
-    pp(values)
+    values_string = sorted(values_string, key=lambda m: m[1])
+    y = [x1[2][0] for x1 in values_string]
+    x = [x1[2][1] for x1 in values_string]
+    y.insert(0, 0)
+    x.insert(0, 0)
+    y.append(1)
+    x.append(1)
 
-    y = [x1[2][0] for x1 in values]
-    x = [x1[2][1] for x1 in values]
+    return x, y
 
-    plt.scatter(x, y, marker="o", color=p.red())
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
+
+def plot_roc_curve(hour, iterations=10, divisions=50):
+    p = Palette()
+    colors = [p.red(), p.blue()]
+    outputs = [OUTPUT_STRING_TIE, OUTPUT_SALMON]
+    for out, color in zip(outputs, colors):
+        data = get_data(hour, out)
+        x, y = get_plot_values(data, iterations, divisions, out)
+        plt.plot(x, y, marker="o", markerfacecolor=p.white(),
+                 color=color)
+
+    plt.xlim(-0.1, 1.1)
+    plt.ylim(0, 1.1)
     plt.plot([0, 1], [0, 1], ls="--", color=p.gray())
-    plt.annotate(f"TPM={round(values[0][0], 2)}",
-                 xy=(x[0], y[0]),
-                 xytext=(x[0] + 0.1, y[0] - 0.1),
-                 arrowprops=dict(arrowstyle="->"))
+    # plt.annotate(f"TPM={round(values_string[0][0], 2)}",
+    #              xy=(x[1], y[1]),
+    #              xytext=(x[1] + 0.1, y[1] - 0.1),
+    #              arrowprops=dict(arrowstyle="->"),
+    #              bbox=dict(boxstyle="round", fc=p.gray(shade=10)))
     plt.ylabel("True Positive Rate")
     plt.xlabel("False Positive Rate")
     plt.title(f"ROC curve at {hour} hpf")
+    plt.grid(axis="both", zorder=10, alpha=0.6)
+
+    leg = [Line2D([0], [0], ls="--", label="Random", color=p.gray())]
+    for label, color in zip(outputs, colors):
+        leg.append(Line2D([0], [0], label=label, color=color))
+    plt.legend(handles=leg, loc="lower right")
     plt.tight_layout()
     plt.savefig("roc.png", type="png", dpi=300)
     plt.show()
 
 
 def run():
-    plot_roc_curve(24)
+    plot_roc_curve(72)
